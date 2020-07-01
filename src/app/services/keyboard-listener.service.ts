56ca9a03f8c4e1883e4ad519abb974922491a829
Dominique Dresen <dominique.dresen@gmail.com>
2020-07-01 19:12:10 +0200

    Add simple search for keyboard input logic

--- src/app/services/keyboard-listener.service.ts
+++ src/app/services/keyboard-listener.service.ts
@@ -9,23 +9,30 @@ export class KeyboardListenerService {
   initialized = false;
   constructor() { }
 
-  init() {
+  init(): void {
     // check if available
     webmidi.default.enable((err) => {
       if (err) {
-        console.log("WebMidi could not be enabled.", err);
+        console.log('WebMidi could not be enabled.', err);
       } else {
-        console.log("WebMidi enabled!");
-        console.log(webmidi.default.inputs);
-        console.log(webmidi.default.outputs);
-          // Listen for a 'note on' message on all channels
-        webmidi.default.inputs[1].addListener('noteon', "all",
-        (e) => {
-          console.log("Received 'noteon' message (" + e.note.name + e.note.octave + ").");
+        console.log('WebMidi enabled!');
+
+        let keyboardInput: webmidi.Input;
+        for (const input of webmidi.default.inputs) {
+          if (!input.name.includes('Midi Through Port')) {
+            keyboardInput = input;
+            break;
+          }
         }
-      );
+        if (keyboardInput) {
+          webmidi.default.inputs[1].addListener('noteon', 'all',
+          (e) => {
+            console.log('Received noteon message (' + e.note.name + e.note.octave + ').');
+          });
+        } else {
+          console.log('Failed to find keyboard.');
+        }
       }
-      
     });
   }
 }
